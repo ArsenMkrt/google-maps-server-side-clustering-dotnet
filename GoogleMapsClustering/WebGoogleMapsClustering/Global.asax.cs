f723c4b690830d2cc64a8aba37785b104da535d7
kunukn <kunukn@gmail.com>
2012-11-04 01:23:51 +0000

    removed unused classes and methods

--- GoogleMapsClustering/WebGoogleMapsClustering/Global.asax.cs
+++ GoogleMapsClustering/WebGoogleMapsClustering/Global.asax.cs
@@ -60,22 +60,7 @@ namespace Kunukn.GooglemapsClustering.WebGoogleMapClustering
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-            //HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
-            //HttpContext.Current.Response.Cache.SetNoStore();
-            //EnableCrossDomainAjaxCall();
-        }
-
-        static void EnableCrossDomainAjaxCall()
-        {
-            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
-            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
-            {
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
-                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
-                HttpContext.Current.Response.End();
-            }
-        }
+        {            
+        }        
     }
 }
